1bec588134df9aad0c01f6cdd951dbe0ce904568
Tani Kaye <1tanikaye@gmail.com>
2021-12-13 23:14:13 -0500

    assignment completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :spices, only: [:index, :create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get '/spices', to: 'spices#index'
+
+  # post '/spices/:id', to: 'spices#create'
+
 end
--- spec/models/title_spec.rb
+++ spec/models/title_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Spice, type: :model do
+RSpec.describe Title, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
